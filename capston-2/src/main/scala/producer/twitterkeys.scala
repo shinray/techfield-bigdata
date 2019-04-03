efa899ffab3b7294817ab5b813b8c430f27a0899
shinray <skuo004@ucr.edu>
2019-04-02 20:19:05 -0400

    started capstone 2, updated gitignore

---
+++ capston-2/src/main/scala/producer/twitterkeys.scala
@@ -0,0 +1,12 @@
+package producer
+
+// twitterProducer_Z7YPAR8
+object twitterkeys {
+
+  // twitter super sekrit keys shh don't tell
+  val CONSUMER_KEY = "WXC50jvoWct4SzHLHHeHI7MzD"
+  val CONSUMER_SECRET = "pDA0JMsoka4RUGnKzwOKJBWxNN98JopCvOc6ymom4u1Ac1DnyP"
+  val ACCESS_TOKEN = "1105476295713087488-W3nJ7Fmty1TekFcFOTWlYr5JB0lxXN"
+  val ACCESS_SECRET = "dXW78QUoHa25WLHWPw2LryU9VCasuXirqIGQxTDzLV0Hc"
+
+}
